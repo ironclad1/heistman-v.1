a7275d594f1f0c90797cd41330ca97ee144213a6
Shashank Sharma <58830690+ironclad1@users.noreply.github.com>
2021-08-16 03:50:11 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
-//module.exports = async (client) => {
- // console.log(`[API] Logged in as ${client.user.username}`);
-//  await client.user.setActivity("Heists", {
-//    type: "STREAMING",//can be LISTENING, WATCHING, PLAYING, STREAMING
-//  });
-//};
+module.exports = async (client) => {
+ 
+   client.user.setPresence({ activities: [{ name: 'with Heists' }], status: 'idle' });
+
+}
+   
