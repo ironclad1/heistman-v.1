0aa5f302d273a2ec3bddae504d0960108938ce12
Shashank Sharma <58830690+ironclad1@users.noreply.github.com>
2021-08-16 03:29:00 +0530

    Update and rename unlock.js to u.js

--- commands/moderation/u.js
+++ commands/moderation/u.js
@@ -1,6 +1,6 @@
 const { MessageEmbed } = require("discord.js")
 module.exports = {
-    name: "unlock",
+    name: "u",
     description: "Unlocks a given channel for a particular role!",
     run: async(client, message, args) => {
         if(!message.member.hasPermission("MANAGE_CHANNELS")) return message.reply("You do not have enough perms to use this cmd!")
