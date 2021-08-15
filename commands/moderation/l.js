38cb29068d1a88b2850d4f8fcd85fcd5093d7ff3
Shashank Sharma <58830690+ironclad1@users.noreply.github.com>
2021-08-16 03:29:02 +0530

    Update and rename lock.js to l.js

--- commands/moderation/l.js
+++ commands/moderation/l.js
@@ -1,6 +1,6 @@
 const { MessageEmbed } = require("discord.js")
 module.exports = {
-    name: "lock",
+    name: "l",
     description: "Locks a given channel for a particular role!",
     run: async(client, message, args) => {
         if(!message.member.hasPermission("MANAGE_CHANNELS")) return message.reply("You do not have enough perms to use this cmd!")
