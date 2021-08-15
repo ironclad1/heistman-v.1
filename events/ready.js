3de047a67c5e7e09c3b6a3393b60d935c0fa7b90
Shashank Sharma <58830690+ironclad1@users.noreply.github.com>
2021-08-16 04:13:01 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,20 +1,4 @@
 module.exports = async (client) => {
   
-  const activities = [
-    { name: 'c!help', type: 'LISTENING' }, 
-    { name: '@Lemon EC', type: 'LISTENING' }
-  ];
-
-  // Update presence
-  client.user.setPresence({ status: 'online', activity: activities[0] });
-
-  let activity = 1;
-
-  // Update activity every 30 seconds
-  setInterval(() => {
-    activities[2] = { name: `${client.guilds.cache.size} servers`, type: 'WATCHING' }; // Update server count
-    activities[3] = { name: `${client.users.cache.size} users`, type: 'WATCHING' }; // Update user count
-    if (activity > 3) activity = 0;
-    client.user.setActivity(activities[activity]);
-    activity++;
-  }, 30000);
+  client.user.setPresence({ activities: [{ name: 'with discord.js' }], status: 'idle' });
+}
