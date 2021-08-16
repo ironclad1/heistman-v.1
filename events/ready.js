7a97a04ca7b9a4b4f68d058357fe1d146c09242a
Shashank Sharma <58830690+ironclad1@users.noreply.github.com>
2021-08-16 11:11:08 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 const mongo = require('../mongo')
 module.exports.run = async(client) => {
     console.log(`${client.user.tag} is now online!`);
-    client.user.setActivity("Heists", {type:"WATCHING"})
+    client.user.setActivity("Heists || .help", {type:"WATCHING"})
     await mongo()
 }
