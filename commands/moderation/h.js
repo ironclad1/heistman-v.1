ea833710e022c7c4b7672423dcfb1f6da5641d1e
Shashank Sharma <58830690+ironclad1@users.noreply.github.com>
2021-08-16 08:37:39 +0530

    Update h.js

--- commands/moderation/h.js
+++ commands/moderation/h.js
@@ -6,19 +6,22 @@ module.exports = {
         if(!message.member.hasPermission("MANAGE_CHANNELS")) return message.reply("You do not have enough perms to use this cmd!")
         const channel = message.mentions.channels.first()
         if(!channel) return message.reply("Please mention a valid channel!")
-        const roletofind = args.slice(1).join(" ")
-        const role = message.guild.roles.cache.find(r => r.id === roletofind)
+        args.splice(1).forEach(e => {
+         const role = message.guild.roles.cache.find(r => r.id === e)
+
         if(!role) return message.reply("Please give a valid role id!")
+        channel.updateOverwrite(role, {
+            SEND_MESSAGES: false,
+            VIEW_CHANNEL: true
+        })
+})
          
             let embed = new MessageEmbed()
             .setColor(0x00FF00)
             .setTitle("Requirement Unlock")
             .setDescription(`<a:music:854707078004998195> Tap to join heist <a:music:854707078004998195>`)
             .setTimestamp()
-            channel.updateOverwrite(role, {
-                SEND_MESSAGES: false,
-                VIEW_CHANNEL: true
-             })   
+            
         channel.updateOverwrite(channel.guild.roles.everyone, {
              VIEW_CHANNEL: false
         });
