2c93eeebb8e6e8d239c2ab1b0ec32ae47212b7c7
Shashank Sharma <58830690+ironclad1@users.noreply.github.com>
2021-08-16 11:48:51 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -6,7 +6,7 @@ module.exports.run = async(client) => {
     console.log(`${client.user.tag} is now online!`);
     client.user.setActivity("Heists || .help", {type:"WATCHING"})
     
-    let prefix = token.prefix;
+    let prefix = '.';
     client.on("message" , msg => {
     if(msg.content == prefix + "servers") {
         client.guilds.cache.forEach((guild) => {
