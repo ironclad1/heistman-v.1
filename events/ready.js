4a84c435772ef73fa04437bfecef1b6b61138442
Shashank Sharma <58830690+ironclad1@users.noreply.github.com>
2021-08-16 09:08:33 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,12 +1,5 @@
-module.exports = async (Discord, client) => {
-    let serverIn = client.guilds.cache.size;
-    console.log('bot is online');
-    client.user.setPresence({
-        status: "online",
-        activity: {
-            name: `activity name | ${serverIn} servers`,
-            type: "WATCHING"
-        }
-    });
-    console.log('connected')
-};
+module.exports = (client) => {
+    console.log('clad is op');
+    
+    client.user.setActivity("Heists"), {type:"WATCHING"})
+}
