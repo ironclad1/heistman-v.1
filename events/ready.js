ef99f02f1cd1567f80097c9524034be96e0a22c0
Shashank Sharma <58830690+ironclad1@users.noreply.github.com>
2021-08-16 07:40:22 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,12 @@
-module.exports = async (client) => {
-  
-  client.user.setPresence({ activities: [{ name: 'with discord.js' }], status: 'idle' });
-}
+module.exports = async (Discord, client) => {
+    let serverIn = client.guilds.cache.size;
+    console.log('bot is online');
+    client.user.setPresence({
+        status: "online",
+        activity: {
+            name: `activity name | ${serverIn} servers`,
+            type: "WATCHING"
+        }
+    });
+    console.log('connected')
+};
