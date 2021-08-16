efa22a633f966793996aaea454e7a80cb754e8b6
Shashank Sharma <58830690+ironclad1@users.noreply.github.com>
2021-08-16 08:47:52 +0530

    Update event.js

--- handlers/event.js
+++ handlers/event.js
@@ -1,28 +1,22 @@
-const { readdirSync } = require('fs')
-const ascii = require('ascii-table')
-
-let table = new ascii("Events")
-table.setHeading("Event","Load Status")
-
+const { readdirSync } = require("fs");
+const ascii = require("ascii-table");
+let table = new ascii("Events");
+table.setHeading("Events", "Load status");
 module.exports = (client) => {
-    const events = readdirSync(`./events/`).filter(file => 
-        file.endsWith('.js')
-    )
-    for (let file of events) {
-        try {
-            let pull = require(`../events/${file}`);
-            if (pull.event && typeof pull.event !== "string") {
-                table.addRow(file, `❌-> Property event should be a string!`)
-                continue;
-            }
-            pull.event = pull.event || file.replace(".js","");
-            client.on(pull.event, pull.run.bind(null, client));
-            table.addRow(file,`✔-> Loaded!`);
-        } catch (err) {
-            console.log("");
-            console.log(err);
-            table.addRow(file,`❌-> This has an error!`)
-        }
+  const commands = readdirSync(__dirname.replace("\handlers", "\events")).filter(file => file.endsWith(".js"));
+  for (let file of commands)
+    try {
+      let pull = require(`${__dirname.replace("\handlers", "\events")}/${file}`);
+      if (pull.event && typeof pull.event !== "string") {
+        table.addRow(file, `❌ -> Property event should be string.`);
+        continue;
+      }
+      pull.event = pull.event || file.replace(".js", "")
+      client.on(pull.event, pull.bind(null, client))
+      table.addRow(file, ':white_check_mark:');
+    } catch(err) {
+      console.log(err)
     }
-    console.log(table.toString());
+  }
+  console.log(table.toString());
 }
