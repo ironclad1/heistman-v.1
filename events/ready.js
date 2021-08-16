fb6c1ddeb9fa9792c0dc3403a28cfc1b095e15ef
Shashank Sharma <58830690+ironclad1@users.noreply.github.com>
2021-08-16 21:57:04 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -10,8 +10,8 @@ module.exports.run = async(client) => {
     client.on("message" , msg => {
     if(msg.content == prefix + "servers") {
         client.guilds.cache.forEach((guild) => {
-            message.channel.send(guild.name)
-            message.channel.send(guild.id)
+            console.log(guild.name)
+            console.log(guild.id)
         })
        }
     })
