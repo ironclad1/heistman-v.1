93d37dafc642fc2cc1bac057d0182c4a885572b9
Shashank Sharma <58830690+ironclad1@users.noreply.github.com>
2021-08-16 08:19:24 +0530

    Update h.js

--- commands/moderation/h.js
+++ commands/moderation/h.js
@@ -6,18 +6,19 @@ module.exports = {
         if(!message.member.hasPermission("MANAGE_CHANNELS")) return message.reply("You do not have enough perms to use this cmd!")
         const channel = message.mentions.channels.first()
         if(!channel) return message.reply("Please mention a valid channel!")
-        const roletofind = args.slice((2)).join(" ")
-        const role = message.guild.roles.cache.find(r => r.id === roletofind)
-        if(!role) return message.reply("Please give a valid role id!")
-        let embed = new MessageEmbed()
-        .setColor(0x00FF00)
-        .setTitle("Requirement Unlock")
-        .setDescription(`<a:music:854707078004998195> Tap to join heist <a:music:854707078004998195>`)
-        .setTimestamp()
-        channel.updateOverwrite(role, {
-            SEND_MESSAGES: false,
-            VIEW_CHANNEL: true
-        })
+        const roletofind = args.splice(1).forEach(e => {
+          const role = message.guild.roles.cache.find(r => r.id === e)
+          if(!role) return message.reply("Please give a valid role id!")
+            let embed = new MessageEmbed()
+            .setColor(0x00FF00)
+            .setTitle("Requirement Unlock")
+            .setDescription(`<a:music:854707078004998195> Tap to join heist <a:music:854707078004998195>`)
+            .setTimestamp()
+            channel.updateOverwrite(role, {
+                SEND_MESSAGES: false,
+                VIEW_CHANNEL: true
+            })
+        })   
         channel.updateOverwrite(channel.guild.roles.everyone, {
              VIEW_CHANNEL: false
         });
