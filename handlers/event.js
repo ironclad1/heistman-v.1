86cfa81295124fda3ac41f22533860706c315eb3
Shashank Sharma <58830690+ironclad1@users.noreply.github.com>
2021-08-16 08:01:20 +0530

    Update event.js

--- handlers/event.js
+++ handlers/event.js
@@ -1,19 +1,28 @@
 const { readdirSync } = require('fs')
 const ascii = require('ascii-table')
 
-const Discord = require('discord.js');
-const client = new Discord.Client();
+let table = new ascii("Events")
+table.setHeading("Event","Load Status")
 
-module.exports= (client, Discord)=>{
-    const load_dir = (dir) =>{
-        const event_files = readdirSync(`./events/`).filter(file => file.endsWith('.js'));
-
-        for (const file of event_files){
-            const event = require(`../events/${file}`)
-            const event_name = file.split('.')[0]
-            client.on(event_name,event.bind(Discord, client));
+module.exports = (client) => {
+    const events = readdirSync(`./events/`).filter(file => 
+        file.endsWith('.js')
+    )
+    for (let file of events) {
+        try {
+            let pull = require(`../events/${file}`);
+            if (pull.event && typeof pull.event !== "string") {
+                table.addRow(file, `❌-> Property event should be a string!`)
+                continue;
+            }
+            pull.event = pull.event || file.replace(".js","");
+            client.on(pull.event, pull.run.bind(null, client));
+            table.addRow(file,`✔-> Loaded!`);
+        } catch (err) {
+            console.log("");
+            console.log(err);
+            table.addRow(file,`❌-> This has an error!`)
         }
     }
-
-    ['client', 'guild'].forEach(e => load_dir(e));
+    console.log(table.toString());
 }
