1f0bfc82706a01e02466556519416a08cf0f3d60
Shashank Sharma <58830690+ironclad1@users.noreply.github.com>
2021-08-16 10:53:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ const client = new Client({
 client.commands = new Collection();
 client.aliases = new Collection();
 
+if(msg.content == prefix + "help"){
+    let help = new Discord.MessageEmbed()
+    .setColor("RANDOM")
+    .setThumbnail(client.user.avatarURL())
+    .setDescription(`<a:notewaaa:876691551168901180> \`.h\` = Before Starting Heist to unlock required roles and viewlock the rest.\n<a:spark:876691042164965376> \`.h #channel roleid1 roleid2 roleid3..so on\`\n<:ineeee:876691337955659786><:ineeee:876691337955659786><:ineeee:876691337955659786><:ineeee:876691337955659786><:ineeee:876691337955659786><:ineeee:876691337955659786><:ineeee:876691337955659786><:ineeee:876691337955659786>\n<a:notewaaa:876691551168901180> \`.r\` = After heist to reset perms\n<a:spark:876691042164965376> \`.r #channel\`\n<:bullet:876693936750624768> JOIN DANK LOVERS [CLICK HERE](https://discord.gg/danklovers)\n<:bullet:876693936750624768> [INVITE ME(https://discord.com/api/oauth2/authorize?client_id=633336606375411723&permissions=322672&scope=bot)`)
+    
+    msg.channel.send(help)
+}
+})
 
 
 
