dff8923eb76297226d625fa6a8c50a89c8f5e844
Shashank Sharma <58830690+ironclad1@users.noreply.github.com>
2021-08-16 08:50:11 +0530

    Update event.js

--- handlers/event.js
+++ handlers/event.js
@@ -1,22 +1,24 @@
 const { readdirSync } = require("fs");
-const ascii = require("ascii-table");
-let table = new ascii("Events");
-table.setHeading("Events", "Load status");
+
 module.exports = (client) => {
-  const commands = readdirSync(__dirname.replace("\handlers", "\events")).filter(file => file.endsWith(".js"));
-  for (let file of commands)
-    try {
-      let pull = require(`${__dirname.replace("\handlers", "\events")}/${file}`);
-      if (pull.event && typeof pull.event !== "string") {
-        table.addRow(file, `❌ -> Property event should be string.`);
-        continue;
-      }
-      pull.event = pull.event || file.replace(".js", "")
-      client.on(pull.event, pull.bind(null, client))
-      table.addRow(file, ':white_check_mark:');
-    } catch(err) {
-      console.log(err)
+
+    const commands = readdirSync(__dirname.replace("\handlers", "\events")).filter(file => file.endsWith(".js"));
+
+    for (let file of commands) {
+
+        try {
+            let pull = require(`${__dirname.replace("\handlers", "\events")}/${file}`);
+
+            if (pull.event && typeof pull.event !== "string") {
+                continue;
+            }
+
+            pull.event = pull.event || file.replace(".js", "")
+
+            client.on(pull.event, pull.run.bind(null, client))
+
+        } catch (err) {
+            console.log('❌ Error excecuting event(s)! This is most likely a problem with your code.')
+        }
     }
-  }
-  console.log(table.toString());
 }
