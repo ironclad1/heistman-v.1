5724019ecdf613c00d42bbd21beb5eed100be5d8
Shashank Sharma <58830690+ironclad1@users.noreply.github.com>
2021-08-16 11:29:36 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 const mongo = require('../mongo')
 const Discord = require('discord.js')
-const command = require('.../command)
+const command = require('../command)
 module.exports.run = async(client) => {
     console.log(`${client.user.tag} is now online!`);
     client.user.setActivity("Heists || .help", {type:"WATCHING"})
