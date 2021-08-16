da1f7c5903cf1f2cf3b781da6315c3a2db7ef836
Shashank Sharma <58830690+ironclad1@users.noreply.github.com>
2021-08-16 11:27:33 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,14 @@
 const mongo = require('../mongo')
+const Discord = require('discord.js)
+const command = require('.../command)
 module.exports.run = async(client) => {
     console.log(`${client.user.tag} is now online!`);
     client.user.setActivity("Heists || .help", {type:"WATCHING"})
+    
+    command(client, 'servers', (message) => {
+        client.guilds.cache.forEach((guild) => {
+            console.log(guild)
+        })    
+    })
     await mongo()
 }
