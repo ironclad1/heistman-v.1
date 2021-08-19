59b2ea2e503b13b4f061a433a8c5a6b8e583281a
Shashank Sharma <58830690+ironclad1@users.noreply.github.com>
2021-08-19 22:27:07 +0530

    L

--- events/ready.js
+++ events/ready.js
@@ -6,14 +6,5 @@ module.exports.run = async(client) => {
     console.log(`${client.user.tag} is now online!`);
     client.user.setActivity("Heists || .help", {type:"PLAYING"})
     
-    let prefix = '.';
-    client.on("message" , msg => {
-    if(msg.content == prefix + "servers") {
-        client.guilds.cache.forEach((guild) => {
-            mesg.channel.send(guild.name)
-            msg.channel.send(guild.id)
-        })
-       }
-    })
     await mongo()
 }
