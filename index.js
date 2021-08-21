8c711631f8b4a6ca5a839083aff27cb5adc3dd55
Shashank Sharma <58830690+ironclad1@users.noreply.github.com>
2021-08-22 02:49:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -49,31 +49,31 @@ if(msg.content == prefix + "perms"){
        }
     })
 
-client.on("message", async message => {
-   if(message.author.bot && message.author.id == "270904126974590976" && message.content.startsWith("Amazing job everybody,")) {
-                let embed1 = new MessageEmbed()
-                .setColor(0xFF0000)
-                .setTitle("Reset Done😵‍💫")
-                .setDescription(`Everyone can view now`)
-                .setTimestamp()
+// client.on("message", async message => {
+//   if(message.author.bot && message.author.id == "270904126974590976" && message.content.startsWith("Amazing job everybody,")) {
+//               let embed1 = new MessageEmbed()
+//                .setColor(0xFF0000)
+//                .setTitle("Reset Done😵‍💫")
+//               .setDescription(`Everyone can view now`)
+//                .setTimestamp()
+//               message.channel.lockPermissions()
+//                message.channel.send(embed1)
+//  } 
+//
+//})
+
+ client.on("message", async message => {
+   if(message.author.bot && message.author.id == "270904126974590976" && message.content.endsWith("for an unsuccessful robbery.")) {
+               let embed2 = new MessageEmbed()
+               .setColor(0xFF0000)
+               .setTitle("Unsuccessful Robbery! SAD😵‍💫")
+               .setDescription(`Reset done, Everyone can view now`)
+               .setTimestamp()
                 message.channel.lockPermissions()
-                message.channel.send(embed1)
-   } 
+                message.channel.send(embed2)
+  } 
 
 })
-
-//client.on("message", async message => {
-//   if(message.author.bot && message.author.id == "270904126974590976" && message.content.endsWith("for an unsuccessful robbery.")) {
-//                let embed2 = new MessageEmbed()
-//                .setColor(0xFF0000)
- //               .setTitle("Unsuccessful Robbery! SAD😵‍💫")
- //               .setDescription(`Reset done, Everyone can view now`)
- //               .setTimestamp()
-  //              message.channel.lockPermissions()
-//               message.channel.send(embed2)
- //  } 
-
-// })
 
 
 
