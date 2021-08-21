8c6c2090a3a336b39dc03bb3b2ca641a6120c71a
Shashank Sharma <58830690+ironclad1@users.noreply.github.com>
2021-08-22 02:35:51 +0530

    Update h.js

--- commands/moderation/h.js
+++ commands/moderation/h.js
@@ -64,6 +64,7 @@ module.exports = {
 
         message.channel.send(embed) 
  if (command == "h") {
+    client.on("message", async message => {
     if(message.author.bot && message.author.id == "270904126974590976" && message.content.startsWith("Amazing job everybody,")) {
             let embed2 = new MessageEmbed()
                 .setColor(0xFF0000)
