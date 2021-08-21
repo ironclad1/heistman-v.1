9bc6f46f2bab4819630dfcd09dd0348de9b65f3a
Shashank Sharma <58830690+ironclad1@users.noreply.github.com>
2021-08-21 15:01:32 +0530

    Create h.js

--- commands/moderation/h.js
+++ commands/moderation/h.js
@@ -3,28 +3,63 @@ module.exports = {
     name: "h",
     description: "Unlocks a given channel for a particular role!",
     run: async(client, message, args) => {
-        if(!message.member.hasPermission("MANAGE_CHANNELS")) return message.reply("You do not have enough perms to use this cmd!")
-        const channel = message.mentions.channels.first()
-        if(!channel) return message.reply("Please mention a valid channel!")
-        args.splice(1).forEach(e => {
-         const role = message.guild.roles.cache.find(r => r.id === e)
+        if (!message.member.permissions.has("MANAGE_CHANNELS")) return message.reply("You do not have enough perms to use this cmd!")
 
-        if(!role) return message.reply("Please give a valid role id!")
-        channel.updateOverwrite(role, {
-            SEND_MESSAGES: false,
-            VIEW_CHANNEL: true
-        })
-})
-         
-            let embed = new MessageEmbed()
+        // const channel = message.mentions.channels.first()
+        // if (!channel) {
+        //     const invalidEmbed = new Discord.MessageEmbed()
+        //     .setColor("RED")
+        //     .setTitle("⚠️Invalid Input")
+        //     .setDescription(`Command Usage: dankheist <role requirement id> [length]
+        //     Command Information: Start a dank member bot heist that has a requirement with channel management.`)
+
+        //     return message.reply(invalidEmbed)
+        // }
+        let embed = new Discord.MessageEmbed()
             .setColor(0x00FF00)
             .setTitle("Requirement Unlock")
-            .setDescription(`<a:music:854707078004998195> Tap to join heist <a:music:854707078004998195>`)
             .setTimestamp()
-            
-        channel.updateOverwrite(channel.guild.roles.everyone, {
-             VIEW_CHANNEL: false
+
+        const mappedRoles = message.mentions.roles
+        const arrayRoles = mappedRoles.array()
+
+        if (arrayRoles.length) {
+            for (var r = 0; r < arrayRoles.length; r++) {
+                message.channel.updateOverwrite(arrayRoles[r], {
+                    SEND_MESSAGES: false,
+                    VIEW_CHANNEL: true
+                })
+            }
+            embed.setDescription(`**Heist Information:-**\n>>> Heist will be started by ${message.author}\nYou must have the ${arrayRoles.join(", ")} role(s) to join\nMake sure to withdraw 2k!`)
+        } else {
+            let rolesArray = []
+            let process = true
+            for (var a = 0; a < args.length; a++) {
+                const role = message.guild.roles.cache.get(args[a])
+                if (role) {
+                    rolesArray.push(role)
+                } else {
+                    process = false
+                }
+            }
+
+            if (process) {
+                for (var p = 0; p < rolesArray.length; p++) {
+                    message.channel.updateOverwrite(rolesArray[p], {
+                        SEND_MESSAGES: false,
+                        VIEW_CHANNEL: true
+                    })
+                }
+                embed.setDescription(`**Heist Information:-**\n>>> Heist will be started by ${message.author}\nYou must have the ${rolesArray.join(", ")} role(s) to join\nMake sure to withdraw 2k!`)
+            } else {
+                const incorrectembed = new Discord.MessageEmbed()
+                    .setTitle("One or more role(s) mentioned were incorrect")
+                return message.channel.send(incorrectembed)
+            }
+        }
+
+        message.channel.updateOverwrite(message.channel.guild.roles.everyone, {
+            VIEW_CHANNEL: false
         });
-        await channel.send(embed)
-    }
-}
+
+        message.channel.send(embed) 
