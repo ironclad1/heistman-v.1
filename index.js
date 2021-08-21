028da2c1ecdd7cfb34892ab269ddda3dd8e227c3
Shashank Sharma <58830690+ironclad1@users.noreply.github.com>
2021-08-22 02:52:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -62,18 +62,18 @@ if(msg.content == prefix + "perms"){
 //
 //})
 
- client.on("message", async message => {
-   if(message.author.bot && message.author.id == "270904126974590976" && message.content.endsWith("for an unsuccessful robbery.")) {
-               let embed2 = new MessageEmbed()
-               .setColor(0xFF0000)
-               .setTitle("Unsuccessful Robbery! SAD😵‍💫")
-               .setDescription(`Reset done, Everyone can view now`)
-               .setTimestamp()
-                message.channel.lockPermissions()
-                message.channel.send(embed2)
-  } 
-
-})
+// client.on("message", async message => {
+//   if(message.author.bot && message.author.id == "270904126974590976" && message.content.endsWith("for an unsuccessful robbery.")) {
+//              let embed2 = new MessageEmbed()
+//               .setColor(0xFF0000)
+//               .setTitle("Unsuccessful Robbery! SAD😵‍💫")
+//               .setDescription(`Reset done, Everyone can view now`)
+//               .setTimestamp()
+//                message.channel.lockPermissions()
+//                message.channel.send(embed2)
+//  } 
+//
+//})
 
 
 
