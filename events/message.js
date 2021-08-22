3fa6176dd88d1a684d62e8bcbf4c540a80a4859a
Shashank Sharma <58830690+ironclad1@users.noreply.github.com>
2021-08-22 12:38:32 +0530

    Update message.js

--- events/message.js
+++ events/message.js
@@ -46,17 +46,5 @@ module.exports.run = async(client, message) => {
     if (command) 
         command.run(client, message, args);
 
-   if (command == "h"){
-       if(message.author.bot && message.author.id == "270904126974590976" && message.content.startsWith("Amazing job everybody,")) {
-               let embed1 = new MessageEmbed()
-                .setColor(0xFF0000)
-                .setTitle("Reset Done😵‍💫")              
-                .setDescription(`Everyone can view now`)
-                .setTimestamp()
-                 message.channel.lockPermissions()
-                 message.channel.send(embed1)
-       } 
-    }
-  
 }
 
