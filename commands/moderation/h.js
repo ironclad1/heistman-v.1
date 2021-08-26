10cf41fe105eb97142571c3b1254c7a6baae5b57
Shashank Sharma <58830690+ironclad1@users.noreply.github.com>
2021-08-26 22:21:24 +0530

    Update h.js

--- commands/moderation/h.js
+++ commands/moderation/h.js
@@ -5,7 +5,7 @@ module.exports = {
     run: async(client, message, args) => {
         
     
-        if (!message.channel.permissionsFor(message.member) return message.reply("You do not have `HEIST STARTER` or `HEIST MANGER` role")
+        if (!message.channel.permissionsFor(message.member)) return message.reply("You do not have `HEIST STARTER` or `HEIST MANGER` role")
 
  
         // const channel = message.mentions.channels.first()
