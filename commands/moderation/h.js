17e0c57904317cbeb5f93567de04b4e30886b42a
Shashank Sharma <58830690+ironclad1@users.noreply.github.com>
2021-08-26 18:49:05 +0530

    Update h.js

--- commands/moderation/h.js
+++ commands/moderation/h.js
@@ -11,7 +11,7 @@ module.exports = {
                for(i=0;i<abc.l;i++){
                
           
-              if (!message.member.roles.cache.some(r=>[i].includes(role.name.includes(abc[i])) ) return message.reply("You do not have `HEIST STARTER` or `HEIST MANGER` role")
+              if (!message.member.roles.cache.some(r=>[i].includes(role.name.includes(abc[i]) ) return message.reply("You do not have `HEIST STARTER` or `HEIST MANGER` role")
         } }  
         // const channel = message.mentions.channels.first()
         // if (!channel) {
