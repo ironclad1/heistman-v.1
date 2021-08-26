c8389243f3f6a7ca0aaa89ff9cdf6a2be2edfe48
Shashank Sharma <58830690+ironclad1@users.noreply.github.com>
2021-08-26 22:20:24 +0530

    Update h.js

--- commands/moderation/h.js
+++ commands/moderation/h.js
@@ -5,14 +5,7 @@ module.exports = {
     run: async(client, message, args) => {
         
     
-     const role = 
-        let i;
-            let abc = ["Heist Manager", "HEIST STARTER"]
-            for(i=0;i<abc.length;i++){
-            if(role.name.includes(abc[i])){
-            if (!message.member.roles.cache.some(r=>["i"].includes(abc[i])) ) return message.reply("You do not have `HEIST STARTER` or `HEIST MANGER` role")
-}        
-      }  
+        if (!message.channel.permissionsFor(message.member) return message.reply("You do not have `HEIST STARTER` or `HEIST MANGER` role")
 
  
         // const channel = message.mentions.channels.first()
