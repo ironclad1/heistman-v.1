2ca7c53747c251974b69061fe354229ec03fb771
Shashank Sharma <58830690+ironclad1@users.noreply.github.com>
2021-08-27 06:57:11 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -4,7 +4,20 @@ const Discord = require('discord.js')
 
 module.exports.run = async(client) => {
     console.log(`${client.user.tag} is now online!`);
-    client.user.setActivity("Heists || .help", {type:"PLAYING"})
+    const arrayOfStatus = [
+        `Over ${client.guilds.cache.size} servers!`,
+        `Heists || .help`,
+        `keeping eye on ${client.users.cache.size} `
+  //  client.user.setActivity("Heists || .help", {type:"PLAYING"})
+   ];
+    let index = 0;
+    setInterval(() => {
+     if(index === arrayOfStatus.length) index=0;
+     const status = arrayOfStatus[index];
+     client.user.setActivity(status, {type:"WATCHING"}).catch(console.error)
+     index++;
+    }, 5000)
+    
     
     await mongo()
 }
