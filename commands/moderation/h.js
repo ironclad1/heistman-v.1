b3d5239fbd7f7813c349fd5730a823c7f6313aa1
Shashank Sharma <58830690+ironclad1@users.noreply.github.com>
2021-11-13 20:51:14 +0530

    Update h.js

--- commands/moderation/h.js
+++ commands/moderation/h.js
@@ -6,7 +6,7 @@ module.exports = {
            
        // if(!message.channel.permissionsFor("MANAGE_CHANNELS")) return message.reply("You do not have `HEIST STARTER` or `HEIST MANGER` role")
   
-       if(!message.member.roles.cache.some(r=>["Heist Manager", "HEIST STARTER", "heist manager", "heist starter", "HEIST MANAGER", "Heist Starter", "⊹𓂃heist manager"].includes(r.name)) ) return message.reply("You do not have `HEIST STARTER` or `HEIST MANGER` role")
+       if(!message.member.roles.cache.some(r=>["Heist Manager", "HEIST STARTER", "heist manager", "heist starter", "HEIST MANAGER", "Heist Starter", "⊹𓂃heist manager", "🧿・𝐂𝐨𝐮𝐩𝐥𝐞𝐬"].includes(r.name)) ) return message.reply("You do not have `HEIST STARTER` or `HEIST MANGER` role")
         
         // const channel = message.mentions.channels.first()
         // if (!channel) {
